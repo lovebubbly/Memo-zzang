532fb839d2dbe40151f09335415243f9a4c6a6ed
Bubblyluv <ch445566@naver.com>
2023-09-29 22:19:02 +0900

    버그의 향연
    버그를 박멸하였다.

--- main.py
+++ main.py
@@ -22,4 +22,22 @@ def read_memo():
     return memos
 
 
+@app.put("/memos/{id}")
+def put_memo(memo_req: memo):
+    for m in memos:
+        if m.id == memo_req.id:
+            m.content = memo_req.content
+            return '메모 수정에 성공했습니다.'
+    return '메모 수정에 실패했습니다.'
+
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id: str):  # memo_id를 str로 받아옴
+    for index, m in enumerate(memos):
+        if m.id == memo_id:  # id만 비교
+            memos.pop(index)  # 해당 인덱스의 메모 삭제
+            return '메모 삭제에 성공했습니다.'
+    return '메모 삭제에 실패했습니다.'
+
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,8 +1,51 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      content: editInput,
+    }),
+  });
+  Readmemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  Readmemo();
+  console.log(`Sending DELETE request for ID: ${id}`);
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
   const li = document.createElement("li");
-  li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  li.innerText = `${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "Edit";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
   ul.appendChild(li);
+  ul.appendChild(editBtn);
+  ul.appendChild(delBtn);
 }
 function handleSubmit(event) {
   event.preventDefault();
